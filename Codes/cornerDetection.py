ed62a8231fb80af3ec3c091d9e28eebc4e656308
RemiDelz <remi.delauzun4@orange.fr>
2021-11-23 16:42:51 +0100

    Ajout vérification appartenance à une ligne

--- Codes/cornerDetection.py
+++ Codes/cornerDetection.py
@@ -15,11 +15,31 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 corners = cv2.goodFeaturesToTrack(gray, 27, 0.01, 10)
 corners = np.int0(corners)
   
-# we iterate through each corner, 
-# making a circle at each point that we think is a corner.
+
+
+line1 = [(79/36),-198.25] # a b
+line2 = [28,-58]
+line3 = [15,20]
+
+lines = [line1,line2,line3]
+
+
 for i in corners:
+    color = (255,0,0)
     x, y = i.ravel()
-    cv2.circle(img, (x, y), 3, 255, -1)
-    print("x:"+str(x)+"  y:"+str(y))
+
+    # test appartenance à une ligne
+    for line in lines:
+        a = line[0]
+        b = line[1]
+
+        if (-a*x+y-b==0):
+            print("le point suivant appartient à une ligne x:"+str(x)+"  y:"+str(y))
+            #cv2.line(img,(1,int(a-b)),(50,int(50*a-b)),(0,255,0),3);
+            color = (0,0,255)
+
+
+    cv2.circle(img, (x, y), 3, color, -1)
 
 plt.imshow(img), plt.show()
+
