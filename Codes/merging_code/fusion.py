7a3007c6dd6ef6317fbce6b0d8dc9e6bad5ff5e4
RemiDelz <remi.delauzun4@orange.fr>
2021-11-25 13:19:51 +0100

    Avancement de fusion.py

--- Codes/merging_code/fusion.py
+++ Codes/merging_code/fusion.py
@@ -13,9 +13,96 @@ import os
 from HoughLineDetection import *
 from cornerDetection import *
 
+radius_point = 3
+cred = (255,0,0)
+cgreen = (0,255,0)
+cblue = (0,0,255)
+cyellow = (0,255,255)
+
+
+def matchLinesPoints(imgPath,liste_lines):
+    img = cv.imread(imgPath)
+
+    points = getHarrisCorner(img)
+    lines = liste_lines
+    linesClean = np.array([])
+
+    pointTrouve = False
+    compteurPointTrouve = 0
+
+    # Passage d'un tableau de ligne defini par deux points...
+    # ...a un tableau de ligne defini par coeff directeur et ordonnee a l'origine
+    for line in lines:
+        linesClean = np.append(linesClean,equationDroite(line),axis=0)    
+    
+    # Mise sous forme de tableau 2 colonnes * N lignes
+    # une ligne du tableau : [a,b] -> coeff directeur, ordonnee a l'origine
+    s = int(linesClean.size/2)
+    linesClean = linesClean.reshape(s,2) 
+    lines = lines.reshape(s,4) # utile pour venir tracer une ligne ensuite en utilisant deux points et non les coeff <a> et <b>
+
+    # A chaque <point> detecte par Harris's corner algorithm, calcule s'il appartient a une des droites de <linesClean>
+    for point in points:
+        for line in linesClean:
+            if appartientDroite(point,line):
+                index = np.where(lines == line) #TODO verifier si c'est bien trouver. Return array de toutes les positions possibles
+                displayPointLine(point,lines[index[0][0]],img,cblue)
+                pointTrouve = True
+                compteurPointTrouve = compteurPointTrouve + 1
+                break
+        
+        # Le point n'a pas ete trouve, il est affiche d'une couleur differente
+        if pointTrouve == False:
+            #displayPoint(point,img,cred)
+            pass
+            
+        
+        pointTrouve = False
+        
+    print(compteurPointTrouve)
+
+def appartientDroite(point,line):
+
+    x = point[0][0]
+    y = point[0][1]
+
+    a = line[0]
+    b = line[1]
+
+    e = y - a*x - b
+    if (e == 0):
+        return True
+    else:
+        return False
+
+
+def displayPointLine(point,line,img,color):
+    """
+    in :
+        point : point {x,y}
+        line : lignes {x1,y1,x2,y2}
+        img : image a afficher
+        color : couleur utilisee pour point et line
+    out:
+
+    Fonction affichant un point et une ligne de la meme couleur
+    """
+    start_point = [line[0],line[1]]
+    end_point = [line[2],line[3]]
+
+    img = cv.circle(img, (point[0][0],point[0][1]), radius_point, color, -1)
+    img = cv.line(img, start_point, end_point, color, 1)
+    cv.imshow("PointLine",img)
+    cv.waitKey()
+
+
+def displayPoint(point,img,color):
+    img = cv.circle(img, (point[0][0],point[0][1]), radius_point, color, -1)
+    cv.imshow("Point",img)
+    cv.waitKey()
+
 
 if __name__ == "__main__":
-    #For all img files in directory dataset
     listOfFiles = os.listdir("Codes/merging_code/dataset")
     for f in listOfFiles:
         if ".png" in f or ".jpg" in f:
@@ -35,5 +122,7 @@ if __name__ == "__main__":
                 new = mergedList.shape
             cleanedList = removeSimilarLines(mergedList,10)
 
+
             displayImgWithLines(imgPath, cleanedList, "Cleaned")
             displayHarrisCorner(imgPath)
+            matchLinesPoints(imgPath, cleanedList)
