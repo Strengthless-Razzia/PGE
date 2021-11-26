b75feef9e030560011bc948e2e081af5d16480c5
Telkan <gamba.gael@orange.fr>
2021-11-26 14:23:21 +0100

    Créer debug mode & Fixes

--- lineDetection/ShapeDetection.py
+++ lineDetection/ShapeDetection.py
@@ -8,18 +8,28 @@ import cv2 as cv
 import numpy as np
 
 
+def pretraitement(img):
+    """
+    in :
+        img : /!\ Pas un path mais bien une image lue avec imread
+    out:
+        imgTraitee : image prétraitée
 
+    Fonction appliquant un prétraitement a l'image afin d'aider la détection de plaque 
+    """
+    kernel = np.ones((5,5),np.uint8)    
+    return cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
 
-
-def getHoughLines(img):
+def getContours(img, debug=False):
     """
     in :
         img : Path vers une image
+        debug : bool qui si True, affiche les différentes parties du traitement
     out:
-        lines : array de N lignes {x1,y1,x2,y2}
+        foundContours : array de forme (N,4,2) avec N le nombre de contours trouvés et 4 sont les 4 angles du rectangle, 2 sont x et y
         Return None si erreur de lecture de l'image
 
-    Fonction générant les lignes Probabilistes de Hough
+    Fonction générant la liste de contours 
     """
     
     # Loads an image
@@ -29,29 +39,39 @@ def getHoughLines(img):
         print ('Error opening image!')
         return None
     
+
     ## TODO Potentiel : Ajouter un prétraitement de l'image pour avoir une meilleure détection des edges
 
     imgEdges = cv.Canny(src, 50, 200, None, 3)
-    cv.imshow("Display",imgEdges)
-    cv.waitKey()
-    contours,h = cv.findContours(imgEdges,cv.RETR_CCOMP  ,cv.CHAIN_APPROX_TC89_L1)
+    if debug:
+        cv.imshow("Original : " + str(img),src)
+        cv.imshow("Edges : " + str(img),imgEdges)
+        cv.waitKey()
+
+    pretreatedImage = pretraitement(imgEdges)
+
+    if debug:
+        cv.imshow("Pretreated : " + str(img),pretreatedImage)
+        cv.waitKey()
     
-    imgEdges = cv.cvtColor(imgEdges,cv.COLOR_GRAY2RGB)
+    contours,h = cv.findContours(pretreatedImage,cv.RETR_CCOMP  ,cv.CHAIN_APPROX_TC89_L1)
+    
+    foundContours = np.empty([0,4,2])
+
     for currentContour in contours:
         if cv.contourArea(currentContour) >200:
             
             tempImg = imgEdges
             approx = cv.approxPolyDP(currentContour, 0.01 * cv.arcLength(currentContour, True), True)
-            if len(approx) == 4:
-                cv.drawContours(tempImg, currentContour, -1, (0, 0, 255),5)
-                for ap in approx:
-                    cv.circle(tempImg,ap[0], 5, (0,255,0), -1)
-                cv.imshow("Kekito",tempImg)
-                cv.waitKey()
+            approx = np.asarray(approx)
+            [x,y,z] = approx.shape
+            approx = np.reshape(approx,(1,x,z))
+            
+            if len(approx[0]) == 4:
+                foundContours = np.vstack([foundContours,approx])
     
+    return foundContours
     
-
-    return contours
 
 
 
@@ -85,7 +105,7 @@ def displayImgWithLines(img, lines, title):
     cv.waitKey()
 
 
-def displayIndividualLinesOfImage(img,lines):
+def displayIndividualContourOfImage(img,lines):
     """
     in :
         img : Path vers une image
@@ -98,10 +118,10 @@ def displayIndividualLinesOfImage(img,lines):
     """
     for line in lines:
         line = line
-        ctr = getCenter(line)
+        
         src = cv.imread(img)
         cv.line(src, (int(line[0]),int(line[1])), (int(line[2]),int(line[3])), (0,0,255), 1, cv.LINE_AA)
-        cv.circle(src,ctr,2,(0,255,0))
+        
         cv.imshow("LineDisplay",src)
         cv.waitKey()
 
@@ -115,6 +135,5 @@ if __name__ == "__main__":
             
             imgPath = "lineDetection\\dataset\\"+f
 
-            lines = getHoughLines(imgPath)
-            #displayImgWithLines(imgPath, lines, "Display")
+            contours = getContours(imgPath, True)
             
