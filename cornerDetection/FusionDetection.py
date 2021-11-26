d1c170e2b7482bb07d55e44495f70100d009abc9
RemiDelz <remi.delauzun4@orange.fr>
2021-11-26 12:43:55 +0100

    contour de la plaque plus précis

--- cornerDetection/FusionDetection.py
+++ cornerDetection/FusionDetection.py
@@ -18,6 +18,8 @@ cred = (0,0,255)
 cgreen = (0,255,0)
 cblue = (255,0,0)
 cyellow = (0,255,255)
+cmarroon = (126,25,126)
+
 
 
 def matchLinesPoints(imgPath,liste_lines):
@@ -26,6 +28,8 @@ def matchLinesPoints(imgPath,liste_lines):
     points = getHarrisCorner(img)
     lines = liste_lines
     linesClean = np.array([])
+    pointsContour = np.array([])
+    linesContour = np.array([])
 
     pointTrouve = False
     compteurPointTrouve = 0
@@ -48,6 +52,7 @@ def matchLinesPoints(imgPath,liste_lines):
             if appartientDroite(point,line):
                 index = np.where(linesClean == line) #TODO verifier si c'est bien trouver. Return array de toutes les positions possibles
                 displayPointLine(point,lines[index[0][0]],img,cblue)
+                [pointsContour,linesContour] = updateContourPlaque(pointsContour,linesContour,point,lines[index[0][0]])
                 pointTrouve = True
                 compteurPointTrouve = compteurPointTrouve + 1
                 break
@@ -59,11 +64,18 @@ def matchLinesPoints(imgPath,liste_lines):
             
         
         pointTrouve = False
-    # linesClean.size/2 car <linesClean> {a,b} (tableau n*2)
-    # points.size/2 car <points> {x,y} (tableau n*2)
-    print(str(compteurPointTrouve) + " points correles avec " + str(int(points.size/2)) + " angles et " + str(int(linesClean.size/2)) + " lignes detectes.")  
-    cv.waitKey()
+        
+    s = int(pointsContour.size/2)
+    pointsContour = pointsContour.reshape(s,2)
+    linesContour = linesContour.reshape(s,2)
+    
+    displayContourPlaque(pointsContour,linesContour,cv.imread(imgPath),cmarroon)
 
+    print(str(compteurPointTrouve) + " points correles avec " + str(int(points.size/2)) + " angles et " + str(int(linesClean.size/2)) + " lignes detectes.")  
+    #print(str(pointsContour) + "\n")
+    #print(str(linesContour) + "\n")
+    cv.waitKey()
+    
 
 
 def appartientDroite(point,line):
@@ -98,10 +110,11 @@ def displayPointLine(point,line,img,color):
     end_point = (int(line[2]),int(line[3]))
     color = (255,0,0)
     img = cv.circle(img, (point[0][0],point[0][1]), radius_point, color, -1)
-    #img = cv.line(img, start_point, end_point, color, 1)
+    img = cv.line(img, start_point, end_point, color, 1)
 
 
     cv.imshow("PointLine",img) 
+    #cv.waitKey()
 
 
 
@@ -118,6 +131,36 @@ def displayPoint(point,img,color):
     img = cv.circle(img, (point[0][0],point[0][1]), radius_point, color, -1)
     cv.imshow("PointLine",img)
 
+
+def updateContourPlaque(liste_points, liste_lines, point, line):
+    # np.append(linesClean,equationDroite(line),axis=0)    
+    # s = int(linesClean.size/2)
+    # linesClean = linesClean.reshape(s,2) 
+    #
+    liste_lines = np.append(liste_lines, equationDroite(line), axis=0)
+    liste_points = np.append(liste_points, point[0], axis=0)
+    return [liste_points,liste_lines]
+    
+
+def displayContourPlaque(liste_points, liste_lines, img, color):
+
+    for point in liste_points:
+        index1 = np.where(liste_points == point)[0][0]
+        # pour chaque point on regarde les autres points...
+        for p in liste_points:
+            index2  = np.where(liste_points == p)[0][0]
+            if index1 != index2:
+                a1 = liste_lines[index1][0]
+                b1 = liste_lines[index1][1]
+                a2 = liste_lines[index2][0]
+                b2 = liste_lines[index2][1]
+
+                # deux points on la meme ligne de reference
+                if (a1==a2) and (b1==b2):
+                    #print(str(int(index1+1)) + " = " + str(int(index2+1)))
+                    img = cv.line(img, (int(point[0]),int(point[1])), (int(p[0]),int(p[1])), color, 2)
+
+    cv.imshow("Contour",img)
 
 
 if __name__ == "__main__":
@@ -141,6 +184,6 @@ if __name__ == "__main__":
             cleanedList = removeSimilarLines(mergedList,10)
 
 
-            displayImgWithLines(imgPath, cleanedList, "Cleaned")
-            displayHarrisCorner(imgPath)
+            #displayImgWithLines(imgPath, cleanedList, "Cleaned")
+            #displayHarrisCorner(imgPath)
             matchLinesPoints(imgPath, cleanedList)
