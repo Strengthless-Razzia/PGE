d7711189e86836804c26d8e1699e88f4c2844c7b
Raphaël <58416668+Strengthless-Razzia@users.noreply.github.com>
2021-12-09 15:15:16 +0100

    ajout du cpp

---
+++ Calibration/cmake-build-debug/CMakeFiles/calibration.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibration.dir/main.cpp.obj"
+  "calibration.exe"
+  "calibration.exe.manifest"
+  "calibration.pdb"
+  "libcalibration.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
