e15020a1d562f4220af57676a992ebd94711217e
Telkan <gamba.gael@orange.fr>
2022-01-24 15:56:05 +0100

    On repère des cylindres maintenant, et on a des points, c'est cool
    Attention, les cylindres sont un peu faux, j'peux pas faire autrement pour l'instant

--- 3D Model Processing/extractHoles.py
+++ 3D Model Processing/extractHoles.py
@@ -3,7 +3,7 @@ import numpy as np
 def extractCircles(file):
     circles = []
     for currentLine in file:
-        if "CIRCLE" in currentLine:
+        if "CYLINDRICAL_SURFACE" in currentLine:
             circles.append(currentLine)
     return circles
 
@@ -15,9 +15,9 @@ def findLineWithId(id, file):
 
 def parseCircle(circle, file, debug=False):
     [id,circle] = circle.split("=")
-    [trash,circle] = circle.split("(")
-    [circle,trash] = circle.split(")")
-    [trash,centerId,diameter] = circle.split(",")
+    [_,circle] = circle.split("(")
+    [circle,_] = circle.split(")")
+    [_,centerId,diameter] = circle.split(",")
         
     foundLine = findLineWithId(centerId, file)
     
@@ -25,8 +25,8 @@ def parseCircle(circle, file, debug=False):
     repereId = parsed[1]
 
     foundLine = findLineWithId(repereId, file)
-    [trash, trash,position] = foundLine.split("(")
-    [position, trash,trash] = position.split(")")
+    [_, _,position] = foundLine.split("(")
+    [position, _,_] = position.split(")")
     [x, y, z] = position.split(",")
 
     
@@ -42,14 +42,13 @@ def parseCircle(circle, file, debug=False):
 
 def getAllCircles(file, getBothFaces=False):
     Circles = extractCircles(file)
-    pos_3D = np.zeros([len(Circles),3])
-    diameters = np.zeros([len(Circles),1])
     results = np.zeros([len(Circles),4])
 
     for i in range(len(Circles)):
         [currentPos, currentD] = parseCircle(Circles[i],file)    
-        results[i][0:3] = currentPos
-        results[i][3] = currentD
+        if currentD < 50:
+            results[i][0:3] = currentPos
+            results[i][3] = currentD
     
     results = np.unique(results,axis=0) 
 
@@ -60,20 +59,21 @@ def getAllCircles(file, getBothFaces=False):
             if results[i][2] == 0:
                 results = np.delete(results,i,axis=0)
             i+=1
-    return results[:,[0,1,2]] , results[:,[3]]
+    return results
+
+
 
 if __name__ == '__main__':
     #Seul code a comprendre, is okay
-    with open('./Data/Plaque1/Model/Plaque_1.stp') as f:
+    with open('./Data/Plaque3/Model/Plaque_3.stp') as f:
         file = f.readlines()
 
-    [points_3D,diameters] = getAllCircles(file, getBothFaces=False) #getBothFaces si vous voulez aussi les points qui correspondent au dessous de la plaque, probablement useless mais sait-on jamais
+    results = getAllCircles(file, getBothFaces=False) #getBothFaces si vous voulez aussi les points qui correspondent au dessous de la plaque, probablement useless mais sait-on jamais
     #ONLY USE POINTS3D, DIAMETERS DONT WORK WOOPSIE
-
-
-    print "positions : ", points_3D.shape
-    print points_3D
-
-    print "diametres : ", diameters.shape
-    print diameters
+    with open('HoleDetection/Points3D/Plaque3.npy', 'wb') as f:
+        np.save(f, results, allow_pickle=False)
+    #print "positions : ", points_3D.shape
+    #print points_3D
+    #print "diametres : ", diameters.shape
+    #print diameters
 
