e5d35ee10f7162befd3dec9ef6f81ddde502f857
Telkan <gamba.gael@orange.fr>
2022-01-25 10:18:04 +0100

    Fix ExtractHoles

--- HoleDetection/extractHoles.py
+++ HoleDetection/extractHoles.py
@@ -3,7 +3,7 @@ import numpy as np
 def extractCircles(file):
     circles = []
     for currentLine in file:
-        if "CIRCLE" in currentLine:
+        if "CYLINDRICAL_SURFACE" in currentLine:
             circles.append(currentLine)
     return circles
 
@@ -15,9 +15,9 @@ def findLineWithId(id, file):
 
 def parseCircle(circle, file, debug=False):
     [id,circle] = circle.split("=")
-    [trash,circle] = circle.split("(")
-    [circle,trash] = circle.split(")")
-    [trash,centerId,diameter] = circle.split(",")
+    [_,circle] = circle.split("(")
+    [circle,_] = circle.split(")")
+    [_,centerId,diameter] = circle.split(",")
         
     foundLine = findLineWithId(centerId, file)
     
@@ -25,8 +25,8 @@ def parseCircle(circle, file, debug=False):
     repereId = parsed[1]
 
     foundLine = findLineWithId(repereId, file)
-    [trash, trash,position] = foundLine.split("(")
-    [position, trash,trash] = position.split(")")
+    [_, _,position] = foundLine.split("(")
+    [position, _,_] = position.split(")")
     [x, y, z] = position.split(",")
 
     
@@ -38,57 +38,42 @@ def parseCircle(circle, file, debug=False):
         print("x y z : ",positionPoint)
     return positionPoint,float(diameter)
 
-def arraysEqual(a,b):
-    return a[0] == b[0] and a[1] == b[1] and a[2] == b[2]
-
-def cleanupSimilar(pos3D, d):
-    i = 0
-    while i < len(pos3D):
-        j=0
-        while j < len(pos3D):
-            if i==j:
-                j+=1
-                continue
-
-            if arraysEqual(pos3D[i], pos3D[j]):
-                pos3D = np.delete(pos3D, j,axis=0)
-                d = np.delete(d, j,axis=0)
-            j+=1
-        i+=1
-    return pos3D,d
 
 
 def getAllCircles(file, getBothFaces=False):
     Circles = extractCircles(file)
-    pos_3D = np.zeros([len(Circles),3])
-    diameters = np.zeros([len(Circles),1])
+    results = np.zeros([len(Circles),4])
 
     for i in range(len(Circles)):
         [currentPos, currentD] = parseCircle(Circles[i],file)    
-        pos_3D[i] = currentPos
-        diameters[i] = currentD
+        if currentD < 50:
+            results[i][0:3] = currentPos
+            results[i][3] = currentD
     
-    pos_3D = np.unique(pos_3D,axis=0) 
+    results = np.unique(results,axis=0) 
 
 
-    #[pos_3D,diameters] = cleanupSimilar(pos_3D,diameters)
     if not getBothFaces:
         i = 0
-        while i < len(pos_3D):
-            if pos_3D[i][2] == 0:
-                pos_3D = np.delete(pos_3D,i,axis=0)
-                diameters = np.delete(diameters,i,axis=0)
+        while i < len(results):
+            if results[i][2] == 0:
+                results = np.delete(results,i,axis=0)
             i+=1
+    return results
 
-    return pos_3D , diameters   
+
 
 if __name__ == '__main__':
     #Seul code a comprendre, is okay
-    with open('./Data/Plaque1/Model/Plaque_1.stp') as f:
+    with open('./Data/Plaque3/Model/Plaque_3.stp') as f:
         file = f.readlines()
 
-    [points_3D,diameters] = getAllCircles(file, getBothFaces=False) #getBothFaces si vous voulez aussi les points qui correspondent au dessous de la plaque, probablement useless mais sait-on jamais
+    results = getAllCircles(file, getBothFaces=False) #getBothFaces si vous voulez aussi les points qui correspondent au dessous de la plaque, probablement useless mais sait-on jamais
     #ONLY USE POINTS3D, DIAMETERS DONT WORK WOOPSIE
+    with open('HoleDetection/Points3D/Plaque3.npy', 'wb') as f:
+        np.save(f, results, allow_pickle=False)
+    #print "positions : ", points_3D.shape
+    #print points_3D
+    #print "diametres : ", diameters.shape
+    #print diameters
 
-    print(points_3D)
-    #print(diameters.shape)
