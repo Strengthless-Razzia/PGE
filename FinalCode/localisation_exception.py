5be31b29e7e38c7f17c581a7e2c5f165d8748033
AurelienMarchal <marchal.aure@gmail.com>
2022-02-14 10:48:17 +0100

    ADDED MATCHING TO FINAL CODE

--- FinalCode/localisation_exception.py
+++ FinalCode/localisation_exception.py
@@ -3,7 +3,7 @@
 #definition des classes erreurs
 class UntrustworthyLocalisationError(Exception):
     def __init__(self, erreur):
-        super(UntrustworthyLocalisationError, self).__init__("La localisation est de mauvaise facture, l'erreur est de " + str(erreur))
+        super(UntrustworthyLocalisationError, self).__init__("La localisation est de mauvaise facture, l'erreur est de {:.2f}".format(erreur))
 
 class MatchingError(Exception):
     def __init__(self, message):
