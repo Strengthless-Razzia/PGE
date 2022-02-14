c0ea2977f864ce29926a8f9deb2792886f120815
Raphaël <58416668+Strengthless-Razzia@users.noreply.github.com>
2022-02-14 14:45:12 +0100

    oui ça marche oui

--- HoleDetection/Paths.py
+++ HoleDetection/Paths.py
@@ -1,4 +1,4 @@
-image = "./Data/Plaque1/Benchmark/image1.bmp"
+image = "./Data/new/1.bmp"
 #image = "./Data/Plaque1/Benchmark/image2.bmp"
 #image = "./Data/Plaque1/Cognex/image1.bmp"
 model = "HoleDetection/Points3D/Plaque1.npy"
